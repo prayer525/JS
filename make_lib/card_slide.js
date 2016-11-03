ad89a7438e25953ba8ea2939bc643d0207aa5706
prayer525 <prayer525@gmail.com>
2016-11-03 17:33:01 +0100

    swipe

--- make_lib/card_slide.js
+++ make_lib/card_slide.js
@@ -12,8 +12,18 @@ var cardSlide = {
 			yDiff:null
 		}
 
+		_this.op = {
+			pageArrow:false,
+			fade:false
+		}
+
+		_this.op = Object.assign(_this.op, _op)
+
 		// _target 아래에 있는 모든 list 를 선택
-		_this.li = document.querySelectorAll(_target);
+		_this.li = document.querySelectorAll(_target + ' li');
+		_this.li.op = _this.op;
+
+		console.log()
 
 		// 첫번째 list 에 class=current(z-index:3) 를 주어 리스트의 맨 앞으로 보낸다.
 		_this.addClass(_this.li[0], 'current');
@@ -29,7 +39,7 @@ var cardSlide = {
 			나누는 숫자(40)가 작을수록 slide 속도가 빨라진다.
 			후에 사용자로부터 입력 받아서 사용 가능하다.
 		*/
-		_this.step = _this.liWidth / 40;
+		_this.step = _this.liWidth / 100;
 
 		/*
 			slide animation 동작중 추가 이벤트를 막기 위한 변수.
@@ -38,21 +48,23 @@ var cardSlide = {
 		*/
 		_this.animation = false;
 
+		// 현재 클릭 event target : fnTouchMove 에서 사용
+		_this.liTarget = null;
+
 		/*
 			width:100% 의 slide 나 반응형 웹일 경우 넓이가 변경이되면
 			slide의 넓이와 넓이에 따라 변화된 step size 를 변경해준다.
 		*/
 		window.onresize = function(){
 			_this.liWidth = _this.li[0].offsetWidth;
-			_this.step = _this.liWidth / 40;
+			_this.step = _this.liWidth / 100;
 		}
 
 		// slide list 의 length : 빈번하게 사용되는 값이라 변수에 저장해서 사용
 		_this.length = _this.li.length;
 
+		// 각 브라우저 별 transform property : webkit , moz , ms 등
 		_this.styleProp = _this.getStyleProperty('transform');
-
-		console.log('_this.styleProp : ', _this.styleProp)
 
 		/*
 			issue : touchmove, left/right animation 시 event.target 의 translate 의 위치값을 변경
@@ -61,15 +73,12 @@ var cardSlide = {
 			modify : 현재 가장 앞에 보여지고 있는 li의 index 번호를 저장.
 					event target 이 아닌 li[index] 로 slide animation 동작
 		*/
-		_this.liIdx = 0;
+		//_this.liIdx = 0;
 
 		// 옵션이 있는경우 : 향후 옵션은 추가 가능
-		if(_op){
-			// pageArrow(좌우 화살표) 가 있으면
-			if(_op.pageArrow){
-				// 좌우 화살표에 event 등록
-				_this.arrowEvent(_target);
-			}
+		if(_this.li.op.pageArrow){
+			// 좌우 화살표에 event 등록
+			_this.arrowEvent(_target);
 		}
 
 		_this.regEvt(_this.li)
@@ -97,9 +106,7 @@ var cardSlide = {
 		}
 	},
 
-	/*
-		좌우 화살표 버튼의 event 등록
-	*/
+	// 좌우 화살표 버튼의 event 등록
 	arrowEvent:function(_target){
 		// 좌우 화살표 버튼의 사용 여부와 함께 좌우 화살표 버튼으로 사용할 className 도 받아서 사용 가능
 		_this.leftArrow = document.querySelector(_target+' .btn-left-arrow');
@@ -114,7 +121,7 @@ var cardSlide = {
 		_this.rightArrow.addEventListener('touchend', _this.fnRightArrow, false);
 	},
 
-	/* touch start, mouse down */
+	// touch start, mouse down
 	fnTouchStart:function(evt){
 		evt.preventDefault();
 		// animation 동작 체크후 동작중이면 return
@@ -127,14 +134,18 @@ var cardSlide = {
 			evt = evt.touches[0];
 		}
 
+		// 현재 이벤트 타겟의 <li /> 를 구한다. : touchmove, touchend 에서 사용
+		_this.liTarget = _this.getIndex(evt.target).el;
+
 		// start position X, Y 좌표 저장
 		_this.point.xDown = evt.clientX;
+		_this.point.yDown = evt.clientY;
 	},
 	/* touch move, mouse move */
 	fnTouchMove:function(evt){
 		evt.preventDefault();
 		// 값이 없을 경우 return
-		if ( ! _this.point.xDown) {
+		if ( ! _this.point.xDown || ! _this.point.yDown ) {
 			return;
 		}
 
@@ -143,25 +154,37 @@ var cardSlide = {
 			evt = evt.touches[0];
 		}
 
+		// slide 동작시 화면의 위 아래 슬라이드를 막기 위해 body 에 스타일 지정
+		document.body.setAttribute('style','overflow:hidden;');
+
 		// start , end position 의 차이를 구한다.
 		_this.point.xDiff = evt.clientX - _this.point.xDown;
+		_this.point.yDiff = evt.clientY - _this.point.yDown;
 
-		_this.li[_this.liIdx].style[_this.styleProp] ='translate3d('+_this.point.xDiff+'px,0px,0px)';
+		// X 축의 변화 값이 Y 축의 변화 값 보다 크면 좌우 슬라이드 동작
+		if ( Math.abs( _this.point.xDiff ) > Math.abs( _this.point.yDiff ) ) {
+			// _this.liTarget : fnTouchStart 에서 저장한 event target
+			_this.liTarget.style[_this.styleProp] = 'translate3d('+_this.point.xDiff+'px,0,0)';
+
+			if(_this.li.op.fade){
+				_this.liTarget.style.opacity = 1- Math.abs(_this.point.xDiff / (_this.liWidth * 2))
+			}
+		}
 	},
 	/* touch end, mouse up */
 	fnTouchEnd:function(evt){
 		evt.preventDefault();
 		// X 축 변화 값이 +100 보다 크면 right slide
 		if(_this.point.xDiff > 100){
-			_this.rightAni(_this.li[_this.liIdx], _this.point.xDiff);
+			_this.rightAni(_this.liTarget, _this.point.xDiff);
 		}
 		// X 축 변화 값이 -100 보다 작으면 left slide
 		else if(_this.point.xDiff < -100){
-			_this.leftAni(_this.li[_this.liIdx], _this.point.xDiff);
+			_this.leftAni(_this.liTarget, _this.point.xDiff);
 		}
 		// X 축 변화 값이 -100 < X < 100 이면 원점으로
 		else{
-			_this.initAni(_this.li[_this.liIdx], _this.point.xDiff);
+			_this.initAni(_this.liTarget, _this.point.xDiff);
 		}
 
 		// 이벤트가 종료 되면 body 의 overflow:hidden 을 제거한다.		
@@ -186,11 +209,8 @@ var cardSlide = {
 		}
 
 		// 현재 class='current' 인 li 를 찾아 slide animation 을 한다.
-		for(var i = 0; i < _this.length; i++){
-			if((_this.li[i].className).indexOf('current') > -1){
-				_this.leftAni(_this.li[i], 0)
-			}
-		}
+		var el = _this.getCurrent(e.target.parentNode)
+		_this.rightAni(el, 0);
 	},
 	// right arrow button event : left button 과 merge 가능
 	fnRightArrow:function(e){
@@ -203,11 +223,8 @@ var cardSlide = {
 		}
 
 		// 현재 class='current' 인 li 를 찾아 slide animation 을 한다.
-		for(var i = 0; i < _this.length; i++){
-			if((_this.li[i].className).indexOf('current') > -1){
-				_this.rightAni(_this.li[i], 0)
-			}
-		}
+		var el = _this.getCurrent(e.target.parentNode)
+		_this.leftAni(el, 0)
 	},
 	// diff position 이 충당 되지 않았을 때 재자리를 찾는 animation
 	initAni:function(_target, currentPoint){
@@ -243,6 +260,7 @@ var cardSlide = {
 
 			// 변경된 값만큼 animation 한다.
 			_target.style[_this.styleProp] ='translate3d('+currentPoint+'px,0px,0px)';
+			_target.style.opacity = 1;
 		}, 10)
 	},
 	// right animation : left animaion 과 통합해서 사용 가능
@@ -254,6 +272,9 @@ var cardSlide = {
 			if(currentPoint < _this.liWidth){
 				currentPoint+=_this.step;
 				_target.style[_this.styleProp] ='translate3d('+currentPoint+'px,0px,0px)';
+				if(_this.li.op.fade){
+					_target.style.opacity = 1- Math.abs(currentPoint / (_this.liWidth * 2))
+				}
 			}
 			// currentPoint 가 li의 넓이보다 클경우 끝까지 이동한걸로 판단 animation 을 종료시키고 list 를 초기화 한다.
 			else{
@@ -265,7 +286,7 @@ var cardSlide = {
 				// animation 상태를 false로 저장
 				_this.animation = false;
 			}
-		}, 10)
+		}, 3)
 	},
 	// left animation : right animation 과 통합해서 사용 가능
 	leftAni:function(_target, currentPoint){
@@ -275,12 +296,15 @@ var cardSlide = {
 			if(currentPoint > -_this.liWidth){
 				currentPoint-=_this.step;
 				_target.style[_this.styleProp] ='translate3d('+currentPoint+'px,0px,0px)';
+				if(_this.li.op.fade){
+					_target.style.opacity = 1- Math.abs(currentPoint / (_this.liWidth * 2))
+				}
 			}else{
 				clearInterval(leftInterval);
 				_this.elementInit(_target);
 				_this.animation = false;
 			}
-		}, 10)
+		}, 3)
 	},
 	// jQuery 의 addClass 에서 착안
 	addClass:function(_t, _cName){
@@ -315,52 +339,45 @@ var cardSlide = {
 	// list element 초기화 : animation 동작 후 실행
 	elementInit:function(_target){
 		// _index 값을 현재 가장 앞에 보이는 list 로 초기화 한다.
-		var _index = _this.liIdx;
+		var _targetInfo = _this.getIndex(_target);
 
 		// 현재 li 의 style 을 제거 한다.
-		_this.li[_index].removeAttribute('style');
+		_target.removeAttribute('style');
 
 		// 현재 li 에서 class='current' 를 제거한다.
-		_this.removeClass(_this.li[_index], 'current')
+		_this.removeClass(_target, 'current')
 
 		// current list 가 last-child 이면
-		if(_index == _this.length-1){
+		if(_targetInfo.liIdx == _targetInfo.parentLi.length-1){
 			// list 의 first-child 에서 next 클래스를 제거하고
-			_this.removeClass(_this.li[0], 'next')
+			_this.removeClass(_targetInfo.parentLi[0], 'next')
 
 			// list 의 first-child 를 current 로 만든다.
-			_this.addClass(_this.li[0], 'current')
+			_this.addClass(_targetInfo.parentLi[0], 'current')
 
 			// list 의 second child 가 다음 list 가 된다.
-			_this.addClass(_this.li[1], 'next')
-
-			// current list 의 index 는 0
-			_this.liIdx = 0;
+			_this.addClass(_targetInfo.parentLi[1], 'next')
 		}
 		// current list 가 last-child 가 아니면
 		else{
 			// current list 의 다음 list 에서 next 를 제거하고
-			_this.removeClass(_this.li[_index+1], 'next')
+			_this.removeClass(_targetInfo.parentLi[_targetInfo.liIdx+1], 'next')
 			// 다음 list 에 current 를 추가한다.
-			_this.addClass(_this.li[_index+1], 'current')
+			_this.addClass(_targetInfo.parentLi[_targetInfo.liIdx+1], 'current')
 			
 			// current 의 다음 list 가 last-child 일 경우
-			if(_index == _this.length - 2){
+			if(_targetInfo.liIdx == _targetInfo.parentLi.length - 2){
 				// first child가 next 가 된다.
-				_this.addClass(_this.li[0], 'next')
+				_this.addClass(_targetInfo.parentLi[0], 'next')
 			}
 			// current 의 다음 list가 last-child 가 아닐 경우
 			else{
 				// 다음 list가 next 가 된다.
-				_this.addClass(_this.li[_index+2], 'next')
+				_this.addClass(_targetInfo.parentLi[_targetInfo.liIdx+2], 'next')
 			}
-
-			// current list 의 index 는 current index + 1
-			_this.liIdx = _index+1;
 		}
 	},
 	getStyleProperty:function(propName, element) {
- 
 		var prefixes = ['Moz', 'Webkit', 'Khtml', 'O', 'Ms'];
 		var _cache = { };
 		element = element || document.documentElement;
@@ -387,5 +404,48 @@ var cardSlide = {
 				return (_cache[propName] = prefixed);
 			}
 		}
+	},
+	getIndex:function(el){
+		var parent = liIdx = null;
+
+		// 현재 el 이 current 클래스를 가지고 있으면 el과 el의 parent 를 반환한다.
+		do{
+			if(el.className.indexOf('current')>-1){
+				parent = el.parentNode;
+				break;
+			}else{
+				el = el.parentNode;
+			}
+		}while(true)
+
+		// <li /> 는 parent 에서 <li /> 선택
+		var parentLi = parent.querySelectorAll('li');
+
+		// current <li /> 의 index 를 반환
+		for(var i=0; i<parentLi.length; i++){
+			if(parentLi[i].className.indexOf('current') > -1){
+				liIdx = i;
+			}
+		}
+
+		/*
+			parentLi : 현재 이벤트가 발생한 <li />의 형재 node,
+			el :current 클래스를 가진 <li />,
+			liIdx : current 클래스를 가진 <li />의 index
+		*/
+		return {liIdx, el, parentLi};
+	},
+	// 좌우 화살표 버튼이 눌러젔을때 current <li />를 찾아 반환한다. : animation target으로 사용된다.
+	getCurrent:function(el){
+		var g = el.querySelectorAll('li');
+		var li = null
+
+		for(var i=0; i<g.length; i++){
+			if(g[i].className.indexOf('current') > -1){
+				li = g[i]
+			}
+		}
+
+		return li;
 	}
 }
