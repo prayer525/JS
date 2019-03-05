e17f16cc5b2d9d9aa2ee36c18ce5f1f47e469887
prayer525 <prayer525@gmail.com>
2019-03-05 20:53:35 +0100

    study

--- study/singlePage/js/singlePage.js
+++ study/singlePage/js/singlePage.js
@@ -1,23 +1,46 @@
 (function( $, undefined ) {
-    $.singlePage = {}
+    // python -m SimpleHTTPServer 8000
+
+    /* 
+        $.ajax : 링크가 걸린 페이지를 ajax로 불러오는 함수
+        e = {
+            url : 'data("href")'
+            ...
+        }
+    */
+
+    $.singlePage = {
+        pageMove:function(e){
+            console.log('getPage : ' , $.singlePage.getPage(e))
+        },
+        getPage:function(e){
+            $('<div></div>').load(e.data('href') + ' > div', function(data){
+                var content = $( $(this).html() );
+
+                content.addClass('ui-page')
+                
+                $('body').append(content)
+            });
+        },
+        createPage:function(e){
+            console.log(typeof e)
+            var el = $(e).find('body').html();
+
+            console.log('e : ' , el)
+        }
+    };
 
     $.extend( $.singlePage, {
 		// define the window and the document objects
 		window: $( window ),
-        document: $( document ),
-        pageMove:function(e){
-            
-        },
-        getPage:function(e){
-
-        },
-        createPage:function(e){
-
-        }
+        document: $( document )
     });
 
+    console.log('aa')
     $(document).off('click', '[data-href]').on('click', '[data-href]', function(){
-        console.log($(this).data())
+        event.preventDefault();
+
+        console.log('element click');
 
         $.singlePage.pageMove($(this));
 
