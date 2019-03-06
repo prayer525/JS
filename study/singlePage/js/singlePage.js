12d44f5b1a27d8c4c1c8f6de9830bab8c30c26e1
prayer525 <prayer525@gmail.com>
2019-03-06 16:20:54 +0100

    create show and hide module

--- study/singlePage/js/singlePage.js
+++ study/singlePage/js/singlePage.js
@@ -1,15 +1,49 @@
 (function( $, undefined ) {
     // python -m SimpleHTTPServer 8000
-
-    /* 
-        $.ajax : 링크가 걸린 페이지를 ajax로 불러오는 함수
-        e = {
-            url : 'data("href")'
-            ...
-        }
-    */
+    $('html').addClass('ui-mobile');
 
     $.singlePage = {
+        whichTransitionEvent:function(){
+            var t,
+            el = document.createElement("fakeelement");
+
+            var transitions = {
+                "transition"      : "transitionend",
+                "OTransition"     : "oTransitionEnd",
+                "MozTransition"   : "transitionend",
+                "WebkitTransition": "webkitTransitionEnd"
+            }
+
+            for (t in transitions){
+                if (el.style[t] !== undefined){
+                    $.singlePage.transition.end = transitions[t];
+                }
+            }
+
+        },
+        whichAnimationEvent:function(){
+            var t,
+                el = document.createElement("fakeelement");
+
+            var animations = {
+                "animation"      : "animationend",
+                "OAnimation"     : "oAnimationEnd",
+                "MozAnimation"   : "animationend",
+                "WebkitAnimation": "webkitAnimationEnd"
+            }
+
+            for (t in animations){
+                if (el.style[t] !== undefined){
+                    $.singlePage.animation.end = animations[t];
+                }
+            }
+        },
+        transition:{
+            end:null
+        },
+        animation:{
+            end:null
+        },
         pageMove:function(e){
             console.log('getPage : ' , $.singlePage.getPage(e))
         },
@@ -17,18 +51,52 @@
             $('<div></div>').load(e.data('href') + ' > div', function(data){
                 var content = $( $(this).html() );
 
-                content.addClass('ui-page')
+                $.singlePage.beforeShowPage(content);
                 
                 $('body').append(content)
             });
         },
-        createPage:function(e){
-            console.log(typeof e)
-            var el = $(e).find('body').html();
+        beforeShowPage:function(content){
+            content.find('[data-role=back]').click(function(){
+                $.singlePage.beforeHidePage(content);
+            })
+            $.singlePage.showPage(content);
+        },
+        showPage:function(content){
+            content.addClass('ui-page ui-page-active slide in');
 
-            console.log('e : ' , el)
+            content.one($.singlePage.transition.end, function(){
+                $.singlePage.afterShowPage(content);
+            });
+
+            content.one($.singlePage.animation.end, function(){
+                $.singlePage.afterShowPage(content);
+            })
+        },
+        afterShowPage:function(content){
+            content.removeClass('slide in');
+        },
+        beforeHidePage:function(content){
+            $.singlePage.hidePage(content);
+        },
+        hidePage:function(content){
+            content.addClass('slide out reverse');
+
+            content.one($.singlePage.transition.end, function(){
+                $.singlePage.afterHidePage(content);
+            });
+
+            content.one($.singlePage.animation.end, function(){
+                $.singlePage.afterHidePage(content);
+            })
+        },
+        afterHidePage:function(content){
+            content.remove();
         }
     };
+
+    $.singlePage.whichTransitionEvent();
+    $.singlePage.whichAnimationEvent();
 
     $.extend( $.singlePage, {
 		// define the window and the document objects
